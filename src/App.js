cc365db0a7e8c592fe7382268e91627b819ce814
Planeshifter <pburckhardt@outlook.com>
2021-07-27 23:53:49 -0400

    Add hooks

--- src/App.js
+++ src/App.js
@@ -1,10 +1,74 @@
-import "./styles.css";
+// MODULES //
 
-export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+import React, { useRef, useEffect, useState } from 'react';
+import { EditorState, EditorView, basicSetup } from '@codemirror/basic-setup';
+import { javascript } from '@codemirror/lang-javascript';
+import './styles.css';
+
+
+// FUNCTIONS //
+
+/**
+ * Hook for the CodeMirror state.
+ * 
+ * @param {string} text - initial text
+ * @returns {Object} CodeMirror editor state
+ */
+function useCodeMirrorState( text ) {
+	const [ editorState, setEditorState ] = useState( null );
+
+	useEffect( () => {
+		setEditorState( EditorState.create({
+			extensions: [
+				basicSetup,
+				javascript()
+			],
+			doc: text
+		}) );
+	}, [] );
+	return editorState;
 }
+
+/**
+ * Hook for the CodeMirror view.
+ * 
+ * @param {Object} editorState - CodeMirror editor state
+ * @returns {Node} CodeMirror dom view
+ */
+function useCodeMirrorView( editorState ) {
+	const dom = useRef( document.createElement( 'div' ) );
+	const [ view, setView ] = useState( null );
+	useEffect( () => {
+		if ( !view && editorState ) {
+			setView( new EditorView({
+				state: editorState, 
+				parent:dom.current
+			}) );
+		}
+	}, [ editorState ] );
+
+	useEffect( () => {
+		if ( view && editorState && view.state !== editorState ) {
+			view.updateState( editorState );
+		}
+	}, [ editorState, view ] );
+	return dom;
+}
+
+
+// MAIN //
+
+function CodePlayground() {
+	const editorState = useCodeMirrorState( 'Hello World!' );
+	const dom = useCodeMirrorView( editorState );
+	return (
+		<div className="CodePlayground">
+			<div ref={dom} />
+		</div>
+	);
+}
+
+
+// EXPORTS //
+
+export default CodePlayground;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import ReactDOM from "react-dom";
 
 import App from "./App";
 
-const rootElement = document.getElementById("root");
+const rootElement = document.getElementById( "root" );
 ReactDOM.render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-  rootElement
+	<StrictMode>
+		<App />
+	</StrictMode>,
+	rootElement
 );
